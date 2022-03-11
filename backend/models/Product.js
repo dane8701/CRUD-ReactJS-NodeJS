1f3a4c11baa5b165917cd4fdb5e602203f3bfa2d
Bryan Dane <kwuimobryan@gmail.com>
2022-03-11 01:27:43 +0100

    Create product model

---
+++ backend/models/Product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  type: { type: String, required: true },
+  price: { type: Number, required: true },
+  rating: { type: Number, required: true },
+  warranty_years: { type: Number, required: true },
+  available: { type: Boolean, required: true }
+});
+
+module.exports = mongoose.model('Product', productSchema);
