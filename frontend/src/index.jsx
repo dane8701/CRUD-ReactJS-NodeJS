d490ab425534c6d073da3d13c89965afcf4abcd5
Bryan Dane <kwuimobryan@gmail.com>
2022-03-11 10:36:16 +0100

    add dependency and file home

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Router>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+          </Routes>
+        </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
